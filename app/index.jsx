5a49a6dac4c613a2b6816a9a7b100eccbe0f7010
frankiecancino <frankiecancino@gmail.com>
2017-03-20 15:47:57 -0500

    added navbar component

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './components/AwesomeComponent.jsx';
+import NavbarComponent from './components/NavbarComponent.jsx';
 
-class App extends React.Component {
-  render () {
+class Nav extends React.Component {
+  render() {
     return (
       <div>
-        <p> Hello React!</p>
-        <AwesomeComponent />
+        <NavbarComponent />
       </div>
     );
   }
 }
 
-render(<App/>, document.getElementById('main'));
+render(<Nav/>, document.getElementById('navbar'));
