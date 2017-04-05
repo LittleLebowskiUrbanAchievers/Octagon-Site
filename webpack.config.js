a6b2f29c6ace11e1d0913feeaa3803c3f1b8ca4a
frankiecancino <frankiecancino@gmail.com>
2017-04-04 23:12:07 -0500

    fixed it hopefully

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 var webpack = require('webpack');
 var path = require('path');
+var app = webpack();
+var port = process.env.PORT || 8000
+let server = require('http').Server(app);
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, '');
+
+app.use(webpack.static(path.join(__dirname)));
+app.get('/', function(req, res, next){
+    res.sendStatus(200);
+});
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 
 var config = {
   entry: APP_DIR + '/index.jsx',
