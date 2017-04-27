7ca2ecdff335d7de16bb93c8aeec5d15cc692fec
frankiecancino <frankiecancino@gmail.com>
2017-04-26 22:48:27 -0500

    fixed react error

--- index.jsx
+++ index.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Router, Route, Link, IndexRoute} from 'react-router';
+import Home from './components/Home.jsx';
+import Predict from './components/Predict.jsx';
 
 class App extends React.Component {
-
+  render() {
+    return (
+      <div></div>
+    );
+  }
 }
 
 export default App;
+
+class HomeComponent extends React.Component {
+  render() {
+    return (
+        <Home />
+    );
+  }
+}
+
+class PredictComponent extends React.Component {
+   render() {
+      return (
+         <Predict />
+      );
+   }
+}
 
 render(<App />, document.getElementById('app'));
